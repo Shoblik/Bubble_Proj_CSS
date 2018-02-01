fc1417f83a21c530bb7efb2dbc5bc5785f0f11cc
Shoblik <shoblik@yahoo.com>
2018-02-01 10:05:53 -0800

    changing up the instructions in the beginning

--- main.js
+++ main.js
@@ -148,60 +148,71 @@ function Makecircles(maxTransTime, maxSize) {
 var launch = new Makecircles(7, 80);
 
 /////////////////////////////////////////////////
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-var count = 0;
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+const instructions = ['Welcome to my particle generator', 'make use of the settings menu to customize your experience', 'finally check out my web portfolio and github'];
+let count = 0;
+while (count < instructions.length) {
+    $('.instructions').text(instructions[count]);
+    setTimeout(() => {
+        count++;
+    }, 1500);
+}
 
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function() {
-        console.log(count++);
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
+// var TxtType = function(el, toRotate, period) {
+//     this.toRotate = toRotate;
+//     this.el = el;
+//     this.loopNum = 0;
+//     this.period = parseInt(period, 10) || 2000;
+//     this.txt = '';
+//     this.tick();
+//     this.isDeleting = false;
+// };
+// var count = 0;
+// TxtType.prototype.tick = function() {
+//     var i = this.loopNum % this.toRotate.length;
+//     var fullTxt = this.toRotate[i];
+//
+//     if (this.isDeleting) {
+//         this.txt = fullTxt.substring(0, this.txt.length - 1);
+//     } else {
+//         this.txt = fullTxt.substring(0, this.txt.length + 1);
+//     }
+//
+//     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+//
+//     var that = this;
+//     var delta = 200 - Math.random() * 100;
+//
+//     if (this.isDeleting) { delta /= 2; }
+//
+//     if (!this.isDeleting && this.txt === fullTxt) {
+//         delta = this.period;
+//         this.isDeleting = true;
+//     } else if (this.isDeleting && this.txt === '') {
+//         this.isDeleting = false;
+//         this.loopNum++;
+//         delta = 500;
+//     }
+//
+//     setTimeout(function() {
+//         that.tick();
+//     }, delta);
+// };
+//
+// window.onload = function() {
+//     var elements = document.getElementsByClassName('typewrite');
+//     for (var i=0; i<elements.length; i++) {
+//         var toRotate = elements[i].getAttribute('data-type');
+//         var period = elements[i].getAttribute('data-period');
+//         if (toRotate) {
+//             console.log(count++);
+//             new TxtType(elements[i], JSON.parse(toRotate), period);
+//         }
+//     }
+//     // INJECT CSS
+//     var css = document.createElement("style");
+//     css.type = "text/css";
+//     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+//     document.body.appendChild(css);
+// };
