092ef70b685dcbdae391be0ce3392517ab135260
Shoblik <shoblik@yahoo.com>
2018-02-01 09:24:38 -0800

    cleaning stuff up and adding in a welcome message

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
 $(document).ready(function () {
-    //// REmove the mouseover to get back to our previous working product
-    // $('.contentWrapper').on('mouseover', function () {
         $('.contentWrapper').on('mousemove', function () {
-            // if (circleLimit < 0) {
-
-
                 launch.newCircle();
                 circleLimit = 0;
-            // } else {
-            //     // circleLimit++;
-            // }
             if (rocketShipOption) {
                 launch.rocketBoost();
             } else {
                 $('.rocket > img').css({'visibility': 'hidden'});
             }
         });
-
-    // });
     $('.navbar').on('mouseover', function () {
         $('.rocket > img').css({'visibility': 'hidden'});
     })
     $('.navbar').on('mouseout', function () {
         $('.rocket > img').css({'visibility': 'visible'});
-    })
-    // shortcut for making everything dissapear on click
-    // $('body').on('click', function() {
-    //     $('.circle').remove();
-    // })
-
+    });
     $('.contentWrapper').on('transitionend', '.circle', function () {
         $(this).remove();
     });
@@ -44,43 +29,43 @@ var circleLimit = 0;
             $('.rocket > img').css({'visibility': 'visible'});
             rocketShipOption = true;
         }
-    })
-    ///////////////////Ball size/////////////////////////////////////////////////
-    $('#ballSizePx').on('click', function () {
-        $(document).keypress(function (e) {
-            if (e.which == 13) {
-                launch.maxSize = $('#ballSizePx').val();
-                $('.ballSize').addClass('selected');
-
-            }
-        });
     });
-    $('#transitionTime').on('click', function () {
-        $(document).keypress(function (e) {
-            if (e.which == 13) {
-                launch.transitionTime = $('#transitionTime').val();
-                $('.transTime').addClass('selected');
-
-            }
-        });
+    ///////////////////Ball size/////////////////////////////////////////////////
+    $('#ballSizePx').on('input', function () {
+        launch.maxSize = $('#ballSizePx').val();
+        $('.ballSize').addClass('selected');
+    });
+    $('#transitionTime').on('input', function () {
+        launch.transitionTime = $('#transitionTime').val();
+        $('.transTime').addClass('selected');
     });
     $('#randomSizeCheckbox').on('click', function() {
         $('.randomSize').toggleClass('selected');
     });
-    $('#transitionDelayInput').on('click', function () {
-        $(document).keypress(function (e) {
-            if (e.which == 13) {
-                launch.transitionDelay = $('#transitionDelayInput').val();
-                $('.transDelay').addClass('selected');
-            }
-        });
+    $('#transitionDelayInput').on('input', function () {
+        launch.transitionDelay = $('#transitionDelayInput').val();
+        $('.transDelay').addClass('selected');
     });
     $('#randomTransitionTime').on('click', function() {
         $('.randomTransTime').toggleClass('selected');
-    })
+    });
     $('#linearTransitionCheckbox').on('click', function() {
         $('.linearTransition').toggleClass('selected');
+    });
+    $('#randomResizeInput').on('input', function() {
+        if ($('#randomResizeCheckbox').is(':checked')) {
+            $('.randomResize').addClass('selected');
+        }
+        launch.maxRandomSizeTransition = $('#randomResizeInput').val();
+    });
+    $('#randomResizeCheckbox').on('click', function() {
+        if ($('#randomResizeCheckbox').is(':checked')) {
+            $('.randomResize').addClass('selected');
+        } else {
+            $('.randomResize').removeClass('selected');
+        }
     })
+
 
 });
 var rocketShipOption = false;
@@ -93,7 +78,6 @@ function Makecircles(maxTransTime, maxSize) {
         return Math.floor(Math.random() * max) + 1;
     };
     this.newCircle = function () {
-
         if ($('#randomSizeCheckbox').is(':checked')) {
             var size = this.size();
         } else {
@@ -109,23 +93,16 @@ function Makecircles(maxTransTime, maxSize) {
         var newCircle = $('<div>').addClass('circle').css({
             'top': event.clientY + 'px',
             'left': event.clientX + 'px',
-            // 'top': '50vh',
-            // 'left': '20%',
             'background-color': 'rgb(' + this.getRandom(255) + ',' + this.getRandom(255) + ',' + this.getRandom(255) + ')',
             'transition': transTime + 's',
             'height': size + 'px',
             'width': size + 'px',
             'transition-delay': this.transitionDelay + 's',
         });
-
         this.circleDiv = newCircle;
-
         this.renderCircleOnDOM(this.circleDiv);
-
         this.setting1(this.circleDiv);
-
-
-    }
+    };
     this.circleDiv = null;
     this.size = function () {
         return this.getRandom(this.maxSize);
@@ -133,11 +110,9 @@ function Makecircles(maxTransTime, maxSize) {
     this.maxRandomSizeTransition = 200;
     this.transitionTime = maxTransTime;
     this.renderCircleOnDOM = function (newCircle) {
-
-        $('.contentWrapper').append(newCircle); //If problems test by making this global
+        $('.contentWrapper').append(newCircle);
     };
     this.setting1 = function (circle) {
-
             if ($('#linearTransitionCheckbox').is(':checked')) {
                 var topTransition = Math.floor(Math.random() * 101) + '%';
             } else {
@@ -153,80 +128,80 @@ function Makecircles(maxTransTime, maxSize) {
             $(circle).css({
                 'transition-timing-function': 'linear',
                 'left': '115%',
-                // 'top': Math.floor(Math.random() * window.screen.height) + 'px',
                 'top': topTransition,
                 'height': size + 'px',
                 'width': size + 'px',
-
-                // 'left':getRandom(window.screen.width) + 'px',
             });
             if ($(circle).css('left') > 114) {
                 $(circle).remove();
             }
         }, 0)
     };
-
     this.rocketBoost = function () {
         $('.rocket').css({
             'top': event.screenY + 'px',
             'left': event.screenX + 'px',
-
         });
     }
-    // this.shootBalls = function() {
-    //     setInterval(function() {
-    //         if ($('.circle').length < 600) {
-    //             this.newCircle();
-    //
-    //         }
-    //     },500);
-    // }
-
-
 }
 
 var launch = new Makecircles(7, 80);
 
-// var launch = new Makecircles(8, 100);
+/////////////////////////////////////////////////
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+var count = 0;
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-// setInterval(function() {
-//     console.log('working');
-//     if ($('.circle').length < 600) {
-//         launch.newCircle();
-//     }
-// }, 500);
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-// function makeCircles() {
-//
-//             var size = getRandom(50);
-//             var newCircle = $('<div>').addClass('circle').css({
-//                 'top': event.clientY + 'px',
-//                 'left': event.clientX + 'px',
-//                 'background-color': 'rgb(' + getRandom(255) + ',' + getRandom(255) + ',' + getRandom(255) + ')',
-//                 'transition': getRandom(7) + 's',
-//                 'height': size + 'px',
-//                 'width': size + 'px',
-//
-//             });
-//             $('body').append(newCircle);
-//             setTimeout(function () {
-//                 mobility(newCircle);
-//             }, 0);
-//
-// }
-//
-// function getRandom(max) {
-//     return Math.floor(Math.random() * max);
-// }
-// function mobility(circle) {
-//     $(circle).css({
-//         'transition-timing-function': 'linear',
-//         'left': '105%',
-//         // 'top': getRandom(window.screen.height) + 'px',
-//         // 'left':getRandom(window.screen.width) + 'px',
-//     })
-//     console.log($(circle).position()['left']/window.screen.width * 100)
-//     if ($(circle).position()['left']/window.screen.width * 100 >= 94) {
-//         $(circle).remove();
-//     }
-// }
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        console.log(count++);
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
